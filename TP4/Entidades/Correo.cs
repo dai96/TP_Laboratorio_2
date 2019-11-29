4dab833aca888d65fb5adb2bd9cd715e9827a5ff
Daira <36193806+dai96@users.noreply.github.com>
2019-11-29 09:52:33 -0300

    Imcompleto
    Falta terminar no llegué

---
+++ TP4/Entidades/Correo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Entidades
+{
+    public class Correo: IMostar<List<Paquete>>
+    {
+        private List<Thread> mockPaquetes;
+        private List<Paquete> paquetes;
+        
+        public List<Paquete> Paquetes
+        {
+            get { return this.paquetes; }
+            set { this.paquetes = value; }
+        }
+        public Correo()
+        {
+            this.mockPaquetes = new List<Thread>();
+            this.Paquetes = new List<Paquete>();
+        }
+        public void FinEntregas()
+        {
+            foreach (Thread item in this.mockPaquetes)
+            {
+                item.Abort();
+            }
+        }
+        public string MostrarDatos(IMostar<List<Paquete>>elementos)
+        {
+            StringBuilder stg = new StringBuilder();
+            stg.AppendFormat("{0} para {1}({2})");
+            return stg.ToString();
+        }
+        public static Correo operator +(Correo c, Paquete p)
+        {
+            foreach (Paquete item in c.paquetes)
+            {
+                if (item == p)
+                {
+
+                }
+            }
+            c.paquetes.Add(p);
+            Thread hilo = new Thread(p.MockCicloDeVida); //creo hilo
+            c.mockPaquetes.Add(hilo);//lo agrego a mockpaquetes
+            hilo.Start();//lo ejecuto
+            return c;
+        }
+    }
+}
---
+++ TP4/Entidades/Paquete.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public delegate void DelegadoEstado(object sender, EventArgs e);
+    public class Paquete: IMostar<Paquete>
+    {
+        private string direccionEntrega;
+        private EEstado estado;
+        private string trackingID;
+
+        public string DireccionEntrega
+        {
+            get { return this.direccionEntrega; }
+            set { this.direccionEntrega = value; }
+        }
+        public EEstado Estado
+        {
+            get { return this.estado; }
+            set { this.estado = value; }
+        }
+        public string TrackingID
+        {
+            get { return this.trackingID; }
+            set { this.trackingID = value; }
+        }
+        public void MockCicloDeVida()
+        {
+
+        }
+        public string MostarDatos(IMostar<Paquete>elemento)
+        {
+            StringBuilder stg = new StringBuilder();
+            stg.AppendFormat("{0} para {1}");
+            return stg.ToString();
+        }
+        public static bool operator ==(Paquete p1, Paquete p2)
+        { }
+        public static bool operator !=(Paquete p1, Paquete p2)
+        { }
+        public Paquete(string direccionEntrega, string trackingID)
+        {
+            this.DireccionEntrega = direccionEntrega;
+            this.TrackingID = trackingID;
+        }
+        public override string ToString()
+        {
+            return this.MostarDatos(this);
+        }
+        public event DelegadoEstado InformaEstado;
+
+        public enum EEstado
+        {
+            Ingresado,
+            EnViaje,
+            Entregado
+        }
+    }
+}
